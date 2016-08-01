9f72a99f2d1564f09d491be4407fdf79bdf8f7a6
Seth Keel <seth.keel@gmail.com>
2016-07-31 23:40:52 -0400

    added .json file for heroku push

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,29 +2,20 @@
 var path = require('path');
 
 
-
 //Routes
-// module.exports = function(app){
-
-// app.get('/', function(req, res){
-//   res.sendFile(path.join(__dirname, './app/public', 'home.html'));
-// });
-
-// app.get('/survey', function(req, res){
-//   res.sendFile(path.join(__dirname, './app/public', 'survey.html'));
-// });
-
-// //Defaults to home.html if no matching route is entered
-// app.use(function(req, res){
-//   res.sendFile(path.join(__dirname, './app/public', 'home.html'));
-// })
-
-// }//End of function(app)
+module.exports = function(app){
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, './app/public', 'home.html'));
+  res.sendFile(path.join(__dirname + '/../public/home.html'));
 });
 
 app.get('/survey', function(req, res){
-  res.sendFile(path.join(__dirname, './app/public', 'survey.html'));
+  res.sendFile(path.join(__dirname + '/../public/survey.html'));
 });
+
+//Defaults to home.html if no matching route is entered
+app.use(function(req, res){
+  res.sendFile(path.join(__dirname + '/../public/home.html'));
+})
+
+}//End of function(app)
